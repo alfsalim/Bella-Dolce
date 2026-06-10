43734d1db5ae59ce87cf2f305d14caf82265fb5c
alfsalim <alfsalim@yahoo.com>
2026-06-10 11:38:38 +0300

    Deploy: 2026-06-10 11:38:38

--- app.config.ts
+++ app.config.ts
@@ -21,7 +21,7 @@ const config = {
 
   // ── Print Agent ────────────────────────────────────────────
   PRINT_AGENT_URL_DEV:  "http://localhost:5555",
-  PRINT_AGENT_URL_PROD: "http://192.168.100.64:5555",
+  PRINT_AGENT_URL_PROD: "http://192.168.8.105:5555",
   PRINT_AGENT_TIMEOUT:  2000,
   PRINT_LANGUAGE: "FR", // USER | BOTH | FR | AR
 
